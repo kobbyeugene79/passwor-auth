0e47fd27e8a16f94e8ae76cc7f6c9d4bda54547d
kobbyeugene79 <73005925+kobbyeugene79@users.noreply.github.com>
2022-11-27 22:48:31 +0000

    done with ThePassword.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+import { ref } from 'vue';
 import Prototype from './components/Prototype.vue';
 import ThePassword from './components/ThePassword.vue';
+
+const passwordInput = ref('')
 
 </script>
 
@@ -9,7 +11,7 @@ import ThePassword from './components/ThePassword.vue';
   <div class="container">
     <h1 class="heading">"Check the <span>Strength</span> of your password"</h1>
     <form @submit.prevent="">
-      <ThePassword v-model="userInput" />
+      <ThePassword v-model="passwordInput" />
     </form>
     <Prototype
       class="Prototype"
